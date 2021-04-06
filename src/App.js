8d97173f469bdc9370b76ecd4692a8d5ef7648ff
[badrri-narayanan] <badrri1995@gmail.com>
2021-04-06 19:08:06 +0530

    start of the HOC learning project

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className='App'>
       <UserList dataSource='https://jsonplaceholder.typicode.com/users' />
       <UserProfile
-        name='Yihua'
-        email='yihuazhang@gmail.com'
+        name='Badrri'
+        email='badrri1995@gmail.com'
         dataSource='https://jsonplaceholder.typicode.com/posts'
       />
     </div>
